7515f5b68015bdbe8f37683031045c072f3c847c
David Schwerd <dmsch656@gmail.com>
2022-02-23 12:43:34 -0700

    GET fetches

--- Develop/server.js
+++ Develop/server.js
@@ -5,3 +5,24 @@ const db = require('./db/db.json');
 const PORT = 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('/assets/js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/assets/js/index.js'));
+});
+app.get('/assets/css/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/assets/css/styles.css'));
+});
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'));
+});
